6555445d034036fac756e9d30d7df17bf419260e
Ratanon090 <Ratanon1@gmail.com>
2024-01-09 00:25:59 +0700

    ฟกฟก

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 const Home = () => {
-  return <div>Hello World!</div>;
+  return <div>
+    <img  src="https://scontent.fbkk29-7.fna.fbcdn.net/v/t1.6435-9/56905101_386981998574601_5594246446942519296_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeE5FOWppQeEMgfsoVKpqRAgVifCMObGrzdWJ8Iw5savN9Ju6MsRd1cCgRA0mJMu3b5PaKu0YecQnRiLvQgErPas&_nc_ohc=mwFoxLYR3BwAX8c5rD8&_nc_ht=scontent.fbkk29-7.fna&oh=00_AfCNNFQZFl_09FFakMbcRqoII2GaOYXP9zzaFMbBAtZ6Uw&oe=65C381BE" width={400} ></img>
+  </div>;
 };
 
 export default Home;
